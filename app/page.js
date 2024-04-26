4ff3dbda17269c84a1440a535ad01600333b8859
shaakilahammed <shaakilahammed@gmail.com>
2024-04-26 22:12:02 +0600

    fix error

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 import LocationDetector from '@/components/LocationDetector';
-import NoLocationFound from '@/components/NoLocationFound';
 import { Suspense } from 'react';
 
 export default function Home() {
     return (
-        <Suspense fallback={<NoLocationFound />}>
+        <Suspense>
             <LocationDetector />
         </Suspense>
     );
