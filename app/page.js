7221b5e51ed44ab6333b61ceea31a4c33380c148
shaakilahammed <shaakilahammed@gmail.com>
2024-04-26 22:06:01 +0600

    fix: vercel deploy

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 import LocationDetector from '@/components/LocationDetector';
+import NoLocationFound from '@/components/NoLocationFound';
+import { Suspense } from 'react';
 
 export default function Home() {
-    return <LocationDetector />;
+    return (
+        <Suspense fallback={<NoLocationFound />}>
+            <LocationDetector />
+        </Suspense>
+    );
 }
